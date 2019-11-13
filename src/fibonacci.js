46a228bf479b2fc747deb4715e50860b675e6bea
Kristina Camia <kcamia19@gmail.com>
2019-11-13 12:55:58 -0500

    idk how but finished fibonacci

---
+++ src/fibonacci.js
@@ -0,0 +1,20 @@
+const readlineSync = require("readline-sync");
+
+const MIN = 1;
+const MAX = 78;
+
+console.log("");
+let input = 0;
+do {
+  input = Number(readlineSync.question("Positive integer: "));
+} while (input < MIN || input > MAX);
+
+let ratioOne = 1.618 ** input;
+let ratioTwo = 0.618 ** input;
+
+let goldenRuleOne = ratioOne - ratioTwo;
+let goldenRuleTwo = goldenRuleOne / 2.236;
+let goldenRule = Math.ceil(goldenRuleTwo);
+let fibonacciNumber = goldenRule.toLocaleString("en");
+
+console.log("\n" + fibonacciNumber + ".")
