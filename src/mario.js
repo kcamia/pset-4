0ff2d499176e2b924ee207b99dfdb5e114ae61d8
Kristina Camia <kcamia19@gmail.com>
2019-11-13 13:18:25 -0500

    start mario

---
+++ src/mario.js
@@ -0,0 +1,12 @@
+const readlineSync = require("readline-sync");
+
+const MIN = 1;
+const MAX = 24;
+
+console.log("")
+let height = 0;
+do {
+  height = readlineSync.question("Height: ")
+
+
+} while (height < MIN || height > MAX);
