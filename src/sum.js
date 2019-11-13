76c19093e836a956407cde35b6883f764c115529
Kristina Camia <kcamia19@gmail.com>
2019-11-13 12:54:06 -0500

    start sum

--- src/sum.js
+++ src/sum.js
@@ -3,13 +3,19 @@ const readlineSync = require("readline-sync");
 const MIN = Number.MIN_SAFE_INTEGER;
 const MAX = Number.MAX_SAFE_INTEGER;
 
+console.log("");
 let lower = -1;
 let upper = 0;
 do {
-  lower = readlineSync.question("\nLower bound: ");
+  lower = readlineSync.question("Lower bound: ");
   upper = readlineSync.question("Upper bound: ");
 } while (lower >= upper || lower < MIN || upper > MAX);
 
-//calculate sum
+let sum = 0;
+do {
+
+} while ();
+
+
 
 console.log("\n" + display + ".")
