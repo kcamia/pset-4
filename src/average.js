6f9ca844a0d899a158e4adacc76eeb49d91dade3
Kristina Camia <kcamia19@gmail.com>
2019-11-15 13:08:11 -0500

    idk

--- src/average.js
+++ src/average.js
@@ -6,10 +6,17 @@ const MAX = Number.MAX_SAFE_INTEGER;
 console.log("");
 let input = 0;
 let sum = 0;
+let amount = 0;
 do {
   input = readlineSync.question("Non-negative integer: ");
 
-    if (input >= 0) {
-      sum = sum + input;
-    }
-} while (input >= 0);
+  if (input !== -1) {
+    sum = sum + input;
+    amount = amount + 1;
+  }
+} while (input !== -1 || !Number.isInteger(input));
+
+let calculate = sum / amount;
+let average = calculate.toLocaleString("en",{minimumFractionDigits: 3, maximumFractionDigits: 3});
+
+console.log("\n" + average + ".")
