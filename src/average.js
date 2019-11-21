579be4be6af1eaf750c77b84f3744851a04157b7
Kristina Camia <kcamia19@gmail.com>
2019-11-21 12:35:12 -0500

    average finish

--- src/average.js
+++ src/average.js
@@ -8,15 +8,14 @@ let input = 0;
 let sum = 0;
 let amount = 0;
 do {
-  input = readlineSync.question("Non-negative integer: ");
-
-  if (input !== -1) {
+  input = Number(readlineSync.question("Non-negative integer: "));
+  if (input >= MIN && input < MAX && Number.isInteger(input)) {
     sum = sum + input;
     amount = amount + 1;
   }
-} while (input !== -1 || !Number.isInteger(input));
+} while (input > 0);
 
-let calculate = sum / amount;
-let average = calculate.toLocaleString("en",{minimumFractionDigits: 3, maximumFractionDigits: 3});
+let average = sum / amount;
+average = average.toLocaleString("en", {minimumFractionDigits: 3, maximumFractionDigits: 3});
 
-console.log("\n" + average + ".")
+console.log("\n" + average + ".");
