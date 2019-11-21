6e9faba71df40adff2700afcd9d5c74217253c83
Kristina Camia <kcamia19@gmail.com>
2019-11-21 12:18:12 -0500

    prime finish

---
+++ src/prime.js
@@ -0,0 +1,27 @@
+const readlineSync = require("readline-sync");
+
+MIN = 0;
+MAX = Number.MAX_SAFE_INTEGER;
+
+console.log("")
+let input = 0;
+do {
+  input = Number(readlineSync.question("Non-negative integer: "));
+} while (input < MIN || input > MAX || !Number.isInteger(input));
+
+let prime = true;
+for (let i = 2; i < input; i = i + 1) {
+  if (input % i == 0) {
+    prime = false;
+  }
+}
+
+if (input == 0) {
+  console.log("\nNot prime.");
+} else if (input == 1) {
+  console.log("\nNot prime.");
+} else if (prime == true) {
+  console.log("\nPrime.")
+} else {
+  console.log("\nNot prime.")
+}
