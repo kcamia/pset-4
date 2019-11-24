89a691f3a091ac61f209f33524cae18346026abd
unknown <kcamia19@gmail.com>
2019-11-24 11:27:31 -0800

    mario finish

--- src/mario.js
+++ src/mario.js
@@ -6,7 +6,27 @@ const MAX = 24;
 console.log("")
 let height = 0;
 do {
-  height = readlineSync.question("Height: ")
+  height = Number(readlineSync.question("Height: "));
 } while (height < MIN || height > MAX || !Number.isInteger(height));
 
-console.log("");
+let length = height + 1;
+let starDisplay = 2;
+console.log();
+for (let row = 0; row < height; row = row + 1) {
+
+    let blank = 1;
+    let star = 1;
+    let display = "";
+    while (blank <= length - starDisplay) {
+        display = display + " ";
+        blank = blank + 1;
+    }
+
+    while (star <= starDisplay) {
+        display = display + "#";
+        star = star + 1;
+    }
+
+    console.log(display);
+    starDisplay = starDisplay + 1;
+}
